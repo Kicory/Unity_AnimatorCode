c8c962ed7ae6908a6048c4345ff728fe96f478fd
안팔아요 <42833717+kicory@users.noreply.github.com>
2019-05-14 10:54:51 +0900

    Add files via upload

---
+++ AnimatorParamHandler.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AnimatorParamHandler : StateMachineBehaviour {
+	[Serializable]
+	public struct ctrlInfo {
+		public enum valueType { Float = 0, Int = 1, Bool = 2, Trigger = 3 };
+		public enum opType { Add, Set, Off, Flip };
+		public valueType type;
+		public opType toDo;
+		public string name;
+		public float floatData;
+		public int intData;
+		public override string ToString() {
+			return type.ToString() + ":" + toDo.ToString() + ":" + name;
+		}
+	}
+	[SerializeField]
+	public List<ctrlInfo> Enter = new List<ctrlInfo>();
+	[SerializeField]
+	public List<ctrlInfo> Leave = new List<ctrlInfo>();
+
+	private bool process(ctrlInfo I, Animator A) {
+		switch(I.type) {
+		case ctrlInfo.valueType.Bool:
+			switch(I.toDo) {
+			case ctrlInfo.opType.Flip:
+				A.SetBool(I.name, !A.GetBool(I.name));
+				return true;
+			case ctrlInfo.opType.Off:
+				A.SetBool(I.name, false);
+				return true;
+			case ctrlInfo.opType.Set:
+				A.SetBool(I.name, true);
+				return true;
+			default:
+				return false;
+			}
+		case ctrlInfo.valueType.Trigger:
+			switch(I.toDo) {
+			case ctrlInfo.opType.Set:
+				A.SetTrigger(I.name);
+				return true;
+			case ctrlInfo.opType.Off:
+				A.ResetTrigger(I.name);
+				return true;
+			default:
+				return false;
+			}
+		case ctrlInfo.valueType.Float:
+			switch(I.toDo) {
+			case ctrlInfo.opType.Add:
+				A.SetFloat(I.name, A.GetFloat(I.name) + I.floatData);
+				return true;
+			case ctrlInfo.opType.Set:
+				A.SetFloat(I.name, I.floatData);
+				return true;
+			default:
+				return false;
+			}
+		case ctrlInfo.valueType.Int:
+			switch(I.toDo) {
+			case ctrlInfo.opType.Add:
+				A.SetInteger(I.name, A.GetInteger(I.name) + I.intData);
+				return true;
+			case ctrlInfo.opType.Set:
+				A.SetInteger(I.name, I.intData);
+				return true;
+			default:
+				return false;
+			}
+		default:
+			return false;
+		}
+	}
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		foreach(var I in Enter) {
+			if (!process(I, animator)) {
+				Debug.LogWarning(I.ToString());
+			}
+		}
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		foreach(var I in Leave) {
+			if(!process(I, animator)) {
+				Debug.LogWarning(I.ToString());
+			}
+		}
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ AnimatorPramHandlerEditor.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+
+[CustomEditor(typeof(AnimatorParamHandler))]
+public class AnimatorParamHandler_Editor : Editor {
+	private ReorderableList listEnter;
+	private ReorderableList listLeave;
+
+	private void OnEnable() {
+		listEnter = new ReorderableList(serializedObject, serializedObject.FindProperty("Enter"), true, true, true, true);
+		listLeave = new ReorderableList(serializedObject, serializedObject.FindProperty("Leave"), true, true, true, true);
+		listEnter.drawElementCallback = (Rect rect, int idx, bool isActive, bool isFocused) => {
+			var elem = listEnter.serializedProperty.GetArrayElementAtIndex(idx);
+			rect.y += 2;
+			var typeProperty = elem.FindPropertyRelative("type");
+			EditorGUI.PropertyField(new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
+				typeProperty, GUIContent.none);
+			EditorGUI.PropertyField(new Rect(rect.x + 60, rect.y, 40, EditorGUIUtility.singleLineHeight),
+				elem.FindPropertyRelative("toDo"), GUIContent.none);
+			EditorGUI.PropertyField(new Rect(rect.x + 100, rect.y, 100, EditorGUIUtility.singleLineHeight),
+				elem.FindPropertyRelative("name"), GUIContent.none);
+			int typeIdx = typeProperty.enumValueIndex;
+			if(typeIdx == 0 || typeIdx == 1) {
+				//float or Int
+				SerializedProperty valueHolder;
+				if(typeIdx == 0) {
+					valueHolder = elem.FindPropertyRelative("floatData");
+				} else {
+					//typeIdx == 1; (int)
+					valueHolder = elem.FindPropertyRelative("intData");
+				}
+				EditorGUI.PropertyField(new Rect(rect.x + 200, rect.y, rect.width - 200, EditorGUIUtility.singleLineHeight),
+					valueHolder, GUIContent.none);
+			}
+		};
+		listLeave.drawElementCallback = (Rect rect, int idx, bool isActive, bool isFocused) => {
+			var elem = listLeave.serializedProperty.GetArrayElementAtIndex(idx);
+			rect.y += 2;
+			var typeProperty = elem.FindPropertyRelative("type");
+			EditorGUI.PropertyField(new Rect(rect.x, rect.y, 60, EditorGUIUtility.singleLineHeight),
+				typeProperty, GUIContent.none);
+			EditorGUI.PropertyField(new Rect(rect.x + 60, rect.y, 40, EditorGUIUtility.singleLineHeight),
+				elem.FindPropertyRelative("toDo"), GUIContent.none);
+			EditorGUI.PropertyField(new Rect(rect.x + 100, rect.y, 100, EditorGUIUtility.singleLineHeight),
+				elem.FindPropertyRelative("name"), GUIContent.none);
+			int typeIdx = typeProperty.enumValueIndex;
+			if(typeIdx == 0 || typeIdx == 1) {
+				//float or Int
+				SerializedProperty valueHolder;
+				if(typeIdx == 0) {
+					valueHolder = elem.FindPropertyRelative("floatData");
+				} else {
+					//typeIdx == 1; (int)
+					valueHolder = elem.FindPropertyRelative("intData");
+				}
+				EditorGUI.PropertyField(new Rect(rect.x + 200, rect.y, rect.width - 200, EditorGUIUtility.singleLineHeight),
+					valueHolder, GUIContent.none);
+			}
+		};
+	}
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+		EditorGUILayout.LabelField("Enter");
+		listEnter.DoLayoutList();
+		EditorGUILayout.LabelField("Leave");
+		listLeave.DoLayoutList();
+		serializedObject.ApplyModifiedProperties();
+	}
+}
